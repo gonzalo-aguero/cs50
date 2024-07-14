5be429011b501fa84fa198fb0e6aa30b56d34449
Gonzalo Agüero <75645989+gonzalo-aguero@users.noreply.github.com>
2024-07-14 17:00:24 +0000

    Updated save model process

--- 05-Neural Networks/digits/handwriting.py
+++ 05-Neural Networks/digits/handwriting.py
@@ -1,6 +1,7 @@
 import sys
 import tensorflow as tf
 
+
 # Use MNIST handwriting dataset
 mnist = tf.keras.datasets.mnist
 
@@ -9,9 +10,11 @@ mnist = tf.keras.datasets.mnist
 x_train, x_test = x_train / 255.0, x_test / 255.0
 y_train = tf.keras.utils.to_categorical(y_train)
 y_test = tf.keras.utils.to_categorical(y_test)
+# Reshape training data
 x_train = x_train.reshape(
     x_train.shape[0], x_train.shape[1], x_train.shape[2], 1
 )
+# Reshape testing data
 x_test = x_test.reshape(
     x_test.shape[0], x_test.shape[1], x_test.shape[2], 1
 )
@@ -21,7 +24,7 @@ model = tf.keras.models.Sequential([
 
     # Convolutional layer. Learn 32 filters using a 3x3 kernel
     tf.keras.layers.Conv2D(
-        32, (3, 3), activation="relu", input_shape=(28, 28, 1)
+        filters=32, kernel_size=(3, 3), activation="relu", input_shape=(28, 28, 1)
     ),
 
     # Max-pooling layer, using 2x2 pool size
@@ -38,6 +41,11 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(10, activation="softmax")
 ])
 
+if len(sys.argv) < 2:
+    print("\033[43m")  # Set background color to yellow
+    print("Warning: No filename provided. The model will not be saved.")
+    print("\033[0m")  # Reset background color
+
 # Train neural network
 model.compile(
     optimizer="adam",
@@ -53,4 +61,12 @@ model.evaluate(x_test,  y_test, verbose=2)
 if len(sys.argv) == 2:
     filename = sys.argv[1]
     model.save(filename)
+    model.save("my_model.keras")
     print(f"Model saved to {filename}.")
+
+"""
+WARNING:absl:You are saving your model as an HDF5 file via `model.save()` or 
+`keras.saving.save_model(model)`. This file format is considered legacy. 
+We recommend using instead the native Keras format, 
+e.g. `model.save('my_model.keras')` or `keras.saving.save_model(model, 'my_model.keras')`. 
+"""
